1e57d5340a5f045e3ee5ce00c79dd2935cc8623d
Tommy Morriss <thmorriss@gmail.com>
2019-07-04 13:13:07 -0700

    made turingmachine object

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-extern crate mini_gl_fb;
-
-use mini_gl_fb::{Config, BufferFormat};
 use mini_gl_fb::glutin::{MouseButton, VirtualKeyCode};
+use mini_gl_fb::{BufferFormat, Config};
 
+use rand::prelude::*;
 use std::time::SystemTime;
 
 const WIDTH: usize = 200;
 const HEIGHT: usize = 200;
 
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
+const LEFT: u8 = 0;
+const RIGHT: u8 = 1;
+const UP: u8 = 2;
+const DOWN: u8 = 3;
+
+
+struct TuringMachine {
+    table: Vec<u8>,
+    map: [u8; WIDTH * HEIGHT],
+    num_states: u8,
+    num_symbols: u8,
+    rng: SmallRng,
+    state: u8,
+    xpos: u8,
+    ypos: u8,
+    itr_count: u32,
 }
 
 /*
@@ -22,16 +32,126 @@ K symbols
 N x K -> N x K x A
 */
 
+impl TuringMachine {
+    fn new(num_states: u8, num_symbols: u8) -> TuringMachine {
+        assert!(num_states >= 1, "must have at least 1 state");
+        assert!(num_symbols >= 2, "must have at least 2 symbols");
+        let mut table = Vec::with_capacity((num_states * num_symbols) as usize * 3);
+        let mut map = [0u8; WIDTH * HEIGHT];
+        let mut rng = SmallRng::from_entropy();
+        let mut state = 0;
+        let mut xpos = 0;
+        let mut ypos = 0;
+        let mut itr_count = 0;
+
+        TuringMachine {
+            table,
+            map,
+            num_states,
+            num_symbols,
+            rng,
+            state,
+            xpos,
+            ypos,
+            itr_count,
+        }
+    }
+
+    fn init(&mut self) {
+        for st in 0..self.num_states {
+            for sy in 0..self.num_symbols {
+                let st1 = self.rng.gen_range(0, self.num_states);
+                let sy1 = self.rng.gen_range(1, self.num_symbols);
+                let ac = self.rng.gen_range(0, 4);
+                self.set_trans(
+                    st,
+                    sy,
+                    st1,
+                    sy1,
+                    ac,
+                );
+            }
+        }
+    }
+
+    fn set_trans(&mut self, st0: u8, sy0: u8, st1: u8, sy1: u8, ac: u8) {
+        let idx = ((self.num_states * sy0 + st0) * 3) as usize;
+
+        self.table[idx + 0] = st1;
+        self.table[idx + 1] = sy1;
+        self.table[idx + 2] = ac;
+    }
+
+    fn reset(&mut self) {
+        self.state = 0;
+        self.ypos = 0;
+        self.xpos = 0;
+        self.itr_count = 0;
+
+        self.map = [0u8; WIDTH * HEIGHT];
+    }
+
+    fn update(&mut self, num_iters: u32) {
+        for i in 0..num_iters {
+            let sy = self.map[WIDTH * (self.ypos + self.xpos) as usize];
+            let st = self.state;
+
+            let idx: usize = (self.num_states * sy as u8 + st as u8) as usize;
+            let st = self.table[idx + 0];
+            let sy = self.table[idx + 1];
+            let ac = self.table[idx + 2];
+
+            self.state = st;
+
+            self.map[WIDTH * (self.ypos + self.xpos) as usize] = sy;
+
+            match ac {
+                LEFT=> {
+                    self.xpos  += 1;
+                    if self.xpos >= WIDTH as u8 {
+                        self.xpos -= WIDTH as u8;
+                    }
+                },
+                RIGHT => {
+                    self.xpos  -= 1;
+                    if self.xpos < 0 {
+                        self.xpos += WIDTH as u8;
+                    }
+                },
+                UP=> {
+                    self.ypos  -= 1;
+                    if self.ypos < 0 {
+                        self.ypos += HEIGHT as u8;
+                    }
+                },
+                DOWN => {
+                    self.ypos += 1;
+                    if self.ypos >= HEIGHT as u8 {
+                        self.ypos -= HEIGHT as u8;
+                    }
+                }
+                _ => panic!("invalid action")
+
+            }
+            self.itr_count += 1;
+        }
+    }
+}
+
 fn main() {
     let mut fb = mini_gl_fb::get_fancy(Config {
-        window_title: "PSA: Conway wants you to appreciate group theory instead",
-        window_size: (800.0, 800.0),
+        window_title: "gaymers",
+        window_size: (WIDTH as _, HEIGHT as _),
         buffer_size: (WIDTH as _, HEIGHT as _),
-        .. Default::default()
+        ..Default::default()
     });
 
     fb.change_buffer_format::<u8>(BufferFormat::R);
     fb.use_post_process_shader(POST_PROCESS);
+
+
+    let mut machine = TuringMachine::new(3,4);
+    machine.init(); //very smart
 
     let mut neighbors = vec![0; WIDTH * HEIGHT];
     let mut cells = vec![false; WIDTH * HEIGHT];
