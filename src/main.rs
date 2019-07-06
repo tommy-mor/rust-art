6ff4f2c8702a3acbdcc1ddb80e1b91c6a54600b1
Christopher Whitman <chris@cwwhitman.com>
2019-07-05 20:57:12 -0700

    yes gaming

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use mini_gl_fb::glutin::{MouseButton, VirtualKeyCode};
 use mini_gl_fb::{BufferFormat, Config};
 
 use rand::prelude::*;
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
 use std::time::SystemTime;
+
+use arrayvec::ArrayVec;
 
 
 const WIDTH: usize = 1024;
@@ -13,23 +20,46 @@ const RIGHT: u8 = 1;
 const UP: u8 = 2;
 const DOWN: u8 = 3;
 
+enum Action {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Distribution<Action> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Action {
+        match rng.gen_range(0, 4) {
+            0 => Action::Up,
+            1 => Action::Down,
+            2 => Action::Left,
+            _ => Action::Right,
+        }
+    }
+}
+
+struct Transition {
+    state: u8,
+    symbol: u8,
+    action: Action
+}
+
 const color_map: [u8; 24] = [
     255,0  ,0  ,    // Initial symbol color
     0  ,0  ,0  ,    // Black
     255,255,255,    // White
     0  ,255,0  ,    // Green
     0  ,0  ,255,    // Blue
-    255,255,0  ,
+    255,255,0  ,    // Yellow
     0  ,255,255,
     255,0  ,255,
 ];
 
 struct TuringMachine {
-    table: Vec<u8>,
+    table: ArrayVec<[Transition; 4096]>,
     map: [u8; WIDTH * HEIGHT],
     num_states: u8,
     num_symbols: u8,
-    rng: SmallRng,
     state: u8,
     xpos: usize,
     ypos: usize,
@@ -47,23 +77,31 @@ impl TuringMachine {
     fn new(num_states: u8, num_symbols: u8) -> TuringMachine {
         assert!(num_states >= 1, "must have at least 1 state");
         assert!(num_symbols >= 2, "must have at least 2 symbols");
-        let mut table = Vec::with_capacity((num_states * num_symbols) as usize * 3);
-        for _ in 0..(num_states * num_symbols * 3) {
-            table.push(0);
-        }
+        assert!((num_states as u16 * num_symbols as u16) <= 4096, "num_states * num_symbols <= 4096");
+
         let mut map = [0u8; WIDTH * HEIGHT];
-        let mut rng = SmallRng::from_entropy();
         let mut state = 0;
         let mut xpos = 0;
         let mut ypos = 0;
         let mut itr_count = 0;
+
+        let mut table = ArrayVec::new();
+        let mut rng = SmallRng::from_entropy();
+        for _ in 0..(num_states as u16 * num_symbols as u16) {
+            let trans = Transition { 
+                state: rng.gen_range(0, num_states),
+                symbol: rng.gen_range(0, num_symbols),
+                action: rng.gen(),
+            };
+            
+            table.push(trans);
+        }
 
         TuringMachine {
             table,
             map,
             num_states,
             num_symbols,
-            rng,
             state,
             xpos,
             ypos,
@@ -71,41 +109,15 @@ impl TuringMachine {
         }
     }
 
-    fn init(&mut self) {
-        for st in 0..self.num_states {
-            for sy in 0..self.num_symbols {
-                let st1 = self.rng.gen_range(0, self.num_states);
-                let sy1 = self.rng.gen_range(1, self.num_symbols);
-                let ac = self.rng.gen_range(0, 4);
-                self.set_trans(
-                    st,
-                    sy,
-                    st1,
-                    sy1,
-                    ac,
-                );
-            }
-        }
-    }
-
     fn get_render_buf(&self) -> Vec<[u8;4]> {
         let mut r_vec = vec![[0u8, 255u8, 255u8, 255u8]; WIDTH * HEIGHT];
-        for i in 0..self.map.len() {
-            let sy = self.map[i];
+        for (sy, rv) in self.map.iter().zip(r_vec.iter_mut()) {
             let r = color_map[(3 * sy + 0) as usize];
             let g = color_map[(3 * sy + 1) as usize];
             let b = color_map[(3 * sy + 2) as usize];
-            r_vec[i] = [r, g, b, 255];
+            *rv = [r, g, b, 255];
         }
         r_vec
-    }
-
-    fn set_trans(&mut self, st0: u8, sy0: u8, st1: u8, sy1: u8, ac: u8) {
-        let idx = ((self.num_states * sy0 + st0) * 3) as usize;
-
-        self.table[idx + 0] = st1;
-        self.table[idx + 1] = sy1;
-        self.table[idx + 2] = ac;
     }
 
     fn reset(&mut self) {
@@ -119,48 +131,40 @@ impl TuringMachine {
 
     fn update(&mut self, num_iters: u32) {
         for i in 0..num_iters {
-            let sadf = (WIDTH * self.ypos + self.xpos);
+            let symbol = &mut self.map[WIDTH * self.ypos + self.xpos];
 
-            let sy = self.map[sadf];
-            let st = self.state;
+            let trans = &self.table[(self.num_states * (*symbol) + self.state) as usize];
+            self.state = trans.state;
 
-            let idx: usize = (self.num_states * sy as u8 + st as u8) as usize;
-            let st = self.table[idx + 0];
-            let sy = self.table[idx + 1];
-            let ac = self.table[idx + 2];
+            *symbol = trans.symbol;
 
-            self.state = st;
-
-            self.map[sadf] = sy;
-
-            match ac {
-                LEFT=> {
+            match trans.action {
+                Action::Left => {
                     self.xpos  += 1;
                     if self.xpos >= WIDTH  {
                         self.xpos -= WIDTH;
                     }
                 },
-                RIGHT => {
+                Action::Right => {
                     self.xpos = if let Some(x) = self.xpos.checked_sub(1) {
                         x
                     } else {
                         WIDTH-1
                     };
                 },
-                UP=> {
-                    if self.ypos == 0 {
-                        self.ypos += HEIGHT - 1;
-                    }
-                    self.ypos  -= 1;
+                Action::Up => {
+                    self.ypos = if let Some(y) = self.ypos.checked_sub(1) {
+                        y
+                    } else {
+                        HEIGHT-1
+                    };
                 },
-                DOWN => {
+                Action::Down => {
                     self.ypos += 1;
                     if self.ypos >= HEIGHT {
                         self.ypos -= HEIGHT;
                     }
                 }
-                _ => panic!("invalid action")
-
             }
             self.itr_count += 1;
         }
@@ -168,19 +172,14 @@ impl TuringMachine {
 }
 
 fn main() {
-    let mut fb = mini_gl_fb::get_fancy(Config {
-        window_title: "gaymers",
-        window_size: (WIDTH as _, HEIGHT as _),
-        buffer_size: (WIDTH as _, HEIGHT as _),
-        ..Default::default()
-    });
+    let mut fb = mini_gl_fb::gotta_go_fast("gaymers", WIDTH as f64, HEIGHT as f64);
 
     fb.change_buffer_format::<u8>(BufferFormat::RGBA);
     //fb.use_post_process_shader(POST_PROCESS);
 
 
-    let mut machine = TuringMachine::new(3,4);
-    machine.init(); //very smart
+    let mut machine = TuringMachine::new(3, 4);
+    //machine.init(); //very smart
 
     let mut previous = SystemTime::now();
     let mut extra_delay: f64 = 0.0;
@@ -214,7 +213,7 @@ fn main() {
             let y = y.min(HEIGHT as f64 - 0.0001).max(0.0).floor() as usize;
 
             machine = TuringMachine::new(3,4);
-            machine.init();
+        //    machine.init();
 //            cells[y * WIDTH + x] = true;
 //            fb.update_buffer(&cells);
 //            // Give the user extra time to make something pretty each time they click
