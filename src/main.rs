a89e917322829edbde486579db794ad3b3d9eae2
Noreply <noreply@github.com>
2019-07-11 23:36:45 +0200

    Experiment with HSV colors

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ use screenshot_rs::screenshot_window;
 const WIDTH: usize = 512;
 const HEIGHT: usize = 512;
 
-const NUM_MACHINES: usize = 16;
+const NUM_MACHINES: usize = 32;
+const STEPS_PER_FRAME: u32 = 10;
 
 enum Action {
     Up,
@@ -253,7 +254,7 @@ fn main() {
         if (seconds > 0.00) && playing {
             previous = SystemTime::now();
             for machine in &mut machines {
-                machine.update(&mut map, 1);
+                machine.update(&mut map, STEPS_PER_FRAME);
             }
             fb.update_buffer(&map[..]);
             println!("frequency {}", 1.0/seconds);
@@ -264,38 +265,18 @@ fn main() {
 }
 
 const COLOR_SYMBOLS: &str = r#"
+
+
+    vec3 hsv2rgb(vec3 c)
+    {
+        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    }
+
+
     void main_image( out vec4 r_frag_color, in vec2 uv )
     {
-        int symbol = int(texture(u_buffer, uv).r * 255);
-        switch (symbol) {
-            case 0:
-                // Red
-                r_frag_color = vec4(255.0, 0.0, 0.0, 1.0);
-                break;
-            case 1:
-                // Black
-                r_frag_color = vec4(0.0, 0.0, 0.0, 1.0);
-                break;
-            case 2:
-                // White
-                r_frag_color = vec4(255.0, 255.0, 255.0, 1.0);
-                break;
-            case 3:
-                // Green
-                r_frag_color = vec4(0.0, 255.0, 0.0, 1.0);
-                break;
-            case 4:
-                // Blue
-                r_frag_color = vec4(0.0, 0.0, 255.0, 1.0);
-                break;
-            case 5:
-                // Yellow
-                r_frag_color = vec4(255.0, 255.0, 0.0, 1.0);
-                break;
-            case 6:
-                // Magenta
-                r_frag_color = vec4(255.0, 0.0, 255.0, 1.0);
-                break;
-        }
+        r_frag_color = vec4(hsv2rgb(vec3(texture(u_buffer, uv).r*10+0.4, 1.0, 1.0)), 1.0);
     }
 "#;
